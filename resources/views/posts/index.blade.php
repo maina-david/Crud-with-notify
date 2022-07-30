7280e8df63a35f0820082faed7de826324da85b8
waihura <waihura.m@gmail.com>
2022-07-30 17:56:31 +0300

    Initial Commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-12">
+            <div class="card">
+                <div class="card-header">{{ __('Posts') }}</div>
+
+                <div class="card-body">
+
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Body</th>
+                            <th>Created At</th>
+                            <th>Updated At</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($posts as $post)
+                            <tr>
+                                <td>{{ $post->title }}</td>
+                                <td>{{ $post->body }}</td>
+                                <td>{{ $post->created_at }}</td>
+                                <td>{{ $post->updated_at }}</td>
+                                <td>
+                                    <a href="{{ route('posts.edit', $post->id) }}" class="btn btn-primary">Edit</a>
+                                    <form action="{{ route('posts.destroy', $post->id) }}" method="POST" class="d-inline">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-danger">Delete</button>
+                                    </form>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
